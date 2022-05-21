2dcdc6f2e9a96f5d411bc2fb745c822b36275e92
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-05-21 13:49:00 +0500

    Прописал вьюшки для страниц posts

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request
 
-posts_blueprint=Blueprint('posts_blueprint', __name__, template_folder='templates')
+posts_blueprint = Blueprint('posts_blueprint', __name__, template_folder='templates')
+
 
 @posts_blueprint.route('/')
 def post_all():
@@ -12,6 +13,11 @@ def post_one(post_id):
     return "Страничка одного поста"
 
 
-@posts_blueprint.route('/')
-def posts_search('/search/'):
+@posts_blueprint.route('/search/')
+def posts_search():
     return "Поиск по постам"
+
+
+@posts_blueprint.route('/users/<username>/')
+def posts_by_user(username):
+    return f"Поиск по имени пользователя {username}"
--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from flask import Flask
 
+from app.posts.views import posts_blueprint
+
 app = Flask(__name__)
+
+app.register_blueprint(posts_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8030)

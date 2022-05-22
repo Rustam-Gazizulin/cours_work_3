d5a82a4073145c49b5a500b76f1faeaec071a903
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-05-22 12:56:07 +0500

    Сделал шаблоны для главной странички

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,15 +1,20 @@
 from flask import Blueprint, render_template, request
+from app.posts.dao.posts_dao import PostsDAO
 
 posts_blueprint = Blueprint('posts_blueprint', __name__, template_folder='templates')
-
+posts_dao = PostsDAO('data/posts.json')
 
 @posts_blueprint.route('/')
 def post_all():
-    return "Все посты тут"
+    try:
+        posts = posts_dao.get_all()
+        return render_template('index.html', posts=posts)
+    except:
+        return "Что пошло не так"
 
 
-@posts_blueprint.route('/posts/<int:post_id>/')
-def post_one(post_id):
+@posts_blueprint.route('/posts/<int:post_pk>/')
+def post_one(post_pk):
     return "Страничка одного поста"
 
 
